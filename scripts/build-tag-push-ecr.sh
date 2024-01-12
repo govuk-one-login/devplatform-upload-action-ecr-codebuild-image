629f0474cf7fec15f07df171a9e655bb933e2da9
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-12 16:32:48 +0000

    Removed SAM functions from GH action

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -8,13 +8,13 @@ fi
 
 echo "Building image"
 
-docker buildx build \
-  --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
-  --output type=image,name="$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA",oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true,push=true "$DOCKER_BUILD_PATH"
+#docker buildx build \
+#  --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
+#  --output type=image,name="$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA",oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true,push=true "$DOCKER_BUILD_PATH"
 
 docker buildx build \
   --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
-  --output type=image,name="$ECR_REGISTRY/$ECR_REPO_NAME:latest",oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true,push=true "$DOCKER_BUILD_PATH"
+  --output type=image,name="$ECR_REGISTRY/$ECR_REPO_NAME:latest,$GITHUB_SHA",oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true,push=true "$DOCKER_BUILD_PATH"
 
 if [ ${CONTAINER_SIGN_KMS_KEY_ARN} != "none" ]; then
     cosign sign --key "awskms:///${CONTAINER_SIGN_KMS_KEY_ARN}" "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
