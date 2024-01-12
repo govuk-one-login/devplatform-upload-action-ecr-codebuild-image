c69a5bb4b59cd8a7e70f40e8398a8ebc82fbdab7
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-12 17:03:42 +0000

    Removed SAM functions from GH action

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -7,7 +7,11 @@ if [ -z $DOCKER_BUILD_PATH ]; then
 fi
 
 echo "Building image"
-
+docker buildx create \
+  --name zstd-builder \
+  --driver docker-container \
+  --driver-opt image=moby/buildkit:v0.12.4
+docker buildx use zstd-builder
 #docker buildx build \
 #  --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
 #  --output type=image,name="$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA",oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true,push=true "$DOCKER_BUILD_PATH"
