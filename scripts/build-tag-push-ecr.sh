a72f71e1fe533878b52107fa09b7d111283de0de
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-15 14:54:26 +0000

    Removed SAM functions from GH action

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -7,18 +7,10 @@ if [ -z $DOCKER_BUILD_PATH ]; then
 fi
 
 echo "Building image"
-docker buildx create \
-  --name zstd-builder \
-  --driver docker-container \
-  --driver-opt image=moby/buildkit:v0.12.4
-docker buildx use zstd-builder
-#docker buildx build \
-#  --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
-#  --output type=image,name="$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA",oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true,push=true "$DOCKER_BUILD_PATH"
 
 docker buildx build -t "$ECR_REGISTRY/$ECR_REPO_NAME:latest" -t "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"\
   --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
-  --output type=registry,oci-mediatypes=true,compression=zstd,compression-level=3,force-compression=true "$DOCKER_BUILD_PATH"
+  --output type=registry,oci-mediatypes=true,force-compression=true "$DOCKER_BUILD_PATH"
 
 if [ ${CONTAINER_SIGN_KMS_KEY_ARN} != "none" ]; then
     cosign sign --key "awskms:///${CONTAINER_SIGN_KMS_KEY_ARN}" "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"
