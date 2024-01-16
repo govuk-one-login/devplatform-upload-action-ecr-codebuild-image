794130bbb64a551787611dd7754c14f582adbb6d
tobias.saunders <tobias.saunders@digital.cabinet-office.gov.uk>
2024-01-16 10:55:17 +0000

    Added compress to the image

--- scripts/build-tag-push-ecr.sh
+++ scripts/build-tag-push-ecr.sh
@@ -10,6 +10,7 @@ echo "Building image"
 
 docker buildx build -t "$ECR_REGISTRY/$ECR_REPO_NAME:latest" -t "$ECR_REGISTRY/$ECR_REPO_NAME:$GITHUB_SHA"\
   --file "$DOCKER_BUILD_PATH"/"$DOCKERFILE" \
+  --compress \
   --output type=registry,oci-mediatypes=true,force-compression=true "$DOCKER_BUILD_PATH"
 
 if [ ${CONTAINER_SIGN_KMS_KEY_ARN} != "none" ]; then
